ee6d073f2efa40e7723b5c2d093bc8bd7c8d6470
Roushan <roushan.khan@outlook.com>
2019-04-07 21:36:26 +0530

    Added codes in the script file to list the attributes of the EC2 instances

--- Scripts/Snapshots.py
+++ Scripts/Snapshots.py
@@ -1,16 +1,20 @@
 import boto3 
-
+import click
 
 session = boto3.Session(profile_name='myprofile')
 ec2 = session.resource('ec2')
-
+# List instances
+@click.command()
 def list_instances():
-        for i in ec2.instances.all():
-            print(i)
+    "List EC2 Instances"
+    for i in ec2.instances.all():
+        print(', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name)))
+    return
 
 if __name__ == "__main__":
     list_instances()
-
-    
-
-
