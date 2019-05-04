c11f8ff5657533ce40b37bfa3f9775b4dbcaff27
Roushan <roushan.khan@outlook.com>
2019-05-04 13:58:39 +0530

    Added setup.py to enable package building and usage

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup (
+    name = 'Snapshots',
+    version = '0.1',
+    author = 'Roushan Khan',
+    author_email = 'roushan.khan@outlook.com',
+    summary = 'Script to manage AWS ec2 volumes snapshots lifecycle',
+    license = 'GPL'
+)
